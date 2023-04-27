fcbe6bc71a3f646db02e8ea4be879c65d99d02e1
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-04-28 00:49:16 +0200

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,7 @@ def safely_get_value(
     dct: Mapping[str, Any], key: str, default: Union[T, None] = None
 ) -> Union[Any, T]:
     """
-    Returns the value in the dictionary with the given key, or the default value
-    if the key is not present.
+    Returns the value in the dictionary with the given key,
+    or the default value if the key is not present.
     """
     return dct.get(key, default)
