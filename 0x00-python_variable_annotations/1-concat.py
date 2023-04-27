26c1f6fe14b011eb63caa9dab2fefd0d5f5401a0
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-04-28 00:06:16 +0200

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Script shows -> Type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Method: takes two str variables & returns their concatenation"""
+    return str1 + str2
