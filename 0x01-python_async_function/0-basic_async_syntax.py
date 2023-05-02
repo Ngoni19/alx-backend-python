c0b9d1f8d7025f02d3914258c5307cc08ac066d7
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-02 02:32:25 +0200

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
-"""Scipt -> Defines an asynchronous coroutine"""
+"""Script -> Defines an asynchronous coroutine"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay"""
-    random_number = random.uniform(0, max_delay)
-    await asyncio.sleep(random_number)
-    return random_number
- 
-def random_delay(max_delay: int) -> Callable[[], float]:
-    """Returns a function that generates a random delay"""
-    return (lambda : random.uniform(0, max_delay))
- 
-async def wait_random_optimized(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay"""
-    rand_func = random_delay(max_delay)
-    random_number = rand_func()
-    await asyncio.sleep(random_number)
-    return random_number
-
+    """Method: Waits for a random delay between 0 & max_delay"""
+    rand_num = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_num)
+    return rand_num
